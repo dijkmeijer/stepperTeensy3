c21b6d3ba4d0aea43a87bfaca89d7ca2c39a2d22
Valentijn <dijkmeijer@gmail.com>
2022-05-03 22:14:55 +0200

    Opschonen commentaar

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include <Arduino.h>
-/**
- * Author Teemu Mäntykallio
- * Initializes the library and runs the stepper
- * motor in alternating directions.
+/*
+ Copyright (c) 2022 Valentijn
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+
+#include <Arduino.h>
+
+
 #define BOARD_DECLS
 #include "board.hpp"
 #define PARSER_DECLS
